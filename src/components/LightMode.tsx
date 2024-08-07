90fbea021fbb6552a10a4374161791274330cdb8
Nipicoco <villarrica43@GMAIL.COM>
2024-08-07 05:57:44 -0400

    asd

--- src/components/LightMode.tsx
+++ src/components/LightMode.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 const LightModeBackground: React.FC = () => {
   return (
     <div className="background-container" style={{ backgroundImage: 'url(assets/lightMode.jpg)', backgroundSize: 'cover' }}>
-      
+      <div className="overlay"></div>
     </div>
   );
 };
--- src/components/NightMode.tsx
+++ src/components/NightMode.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 const NightModeBackground: React.FC = () => {
   return (
     <div className="background-container" style={{ backgroundImage: 'url(assets/nightMode.jpg)', backgroundSize: 'cover' }}>
-      
+      <div className="overlay"></div>
     </div>
   );
 };
